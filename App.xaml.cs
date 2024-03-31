3c1508514b690acd5164cc4786a2bfa377ac2e5d
JoshHollenbeck <162065405+JoshHollenbeck@users.noreply.github.com>
2024-03-31 15:37:03 -0400

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,20 @@
 ﻿using System.Windows;
+using System.Threading;
+using System.Globalization;
 using MainClient._ViewModel;
 using MainClient.Services;
 using MainClient._View;
 
 namespace MainClient
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         private void OnStartup(object sender, StartupEventArgs e)
         {
+            CultureInfo culture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
             // Initialize the DialogService
             IDialogService dialogService = new DialogService();
 
